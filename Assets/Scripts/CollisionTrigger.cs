f226908ec0658590850c9b6e85bcaee64116074e
Vaccie\Tommi <tommi.alaoja@outlook.com>
2022-09-20 10:46:01 +0300

    Minimap konsepti & Kartan vaihteluja. Lajittelin asioita kans vähän kansiohin selveentääkseen ne.

--- Assets/Scripts/CollisionTrigger.cs
+++ Assets/Scripts/CollisionTrigger.cs
@@ -4,11 +4,13 @@ using UnityEngine.SceneManagement;
 
 public class CollisionTrigger : MonoBehaviour
 {
+    #region Variables
     [SerializeField]
     public string scenePath;
     [Range(0, 1)] public int active = 0;
     public GameObject interractionMenu;
     public GameObject Kettu1_Final;
+    #endregion
 
     private void OnCollisionEnter2D(Collision2D other)
     {
